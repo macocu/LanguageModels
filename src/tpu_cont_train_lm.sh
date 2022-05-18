d95e246e79f3eba7feba2fddf83933b334a174f5
RikVN <p266548@colossus.hpc.rug.nl>
2022-05-18 16:19:53 +0200

    tpu training

--- src/tpu_cont_train_lm.sh
+++ src/tpu_cont_train_lm.sh
@@ -15,10 +15,8 @@ config_sh=$1 # Config sh file with experimental settings
 source $config_sh # Load all variables to here
 
 # Set variables
-num_processes=48
+num_processes=8
 SEED=2810
 
 # Do the TPU training from the model
-#python transformers/examples/pytorch/xla_spawn.py --num_cores 8 
-
-python src/run_mlm.py --preprocessing_num_workers $num_processes --model_name_or_path $model_name $max_steps --train_file $train_file --max_seq_length $max_seq_length $line_by_line --output_dir $output_dir $do_train --per_device_train_batch_size $batch_train $overwrite_cache $overwrite_output_dir --gradient_accumulation_steps $gradient_accumulation_steps --save_steps $save_steps --pad_to_max_length --seed $SEED --logging_steps $logging_steps --learning_rate $learning_rate $warmup_ratio $piece_masking
+python transformers/examples/pytorch/xla_spawn.py --num_cores 8 src/run_mlm.py --preprocessing_num_workers $num_processes --model_name_or_path $model_name $max_steps --train_file $train_file --max_seq_length $max_seq_length $line_by_line --output_dir $output_dir $do_train --per_device_train_batch_size $batch_train $overwrite_cache $overwrite_output_dir --gradient_accumulation_steps $gradient_accumulation_steps --save_steps $save_steps --pad_to_max_length --seed $SEED --logging_steps $logging_steps --learning_rate $learning_rate $warmup_ratio $piece_masking
